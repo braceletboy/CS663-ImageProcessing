fd3769abfe8d2f25690c017cebfce2eff980d1ef
deraklmc <gderaj98@gmail.com>
2019-09-02 19:59:41 +0530

    tuning  images

--- assignment-2-filtering/2/code/myMainScript.m
+++ assignment-2-filtering/2/code/myMainScript.m
@@ -6,10 +6,10 @@ tic;
 %% Your code here
 img1 = load('../data/barbara.mat');
 img = double(img1.imageOrig);
-[~] = myBilateralFiltering(img, 0.9, 25, 57);
+[~] = myBilateralFiltering(img, 0.99, 24.2, 57);
 img = imread('../data/grass.png');
-[~] = myBilateralFiltering(img, 7, 26, 3);
+[~] = myBilateralFiltering(img, 7, 42, 3);
 img = imread('../data/honeycombReal.png');
-[~] = myBilateralFiltering(img, 1.8, 30, 13);
+[~] = myBilateralFiltering(img, 2.2, 44, 13);
 %%
 toc;
