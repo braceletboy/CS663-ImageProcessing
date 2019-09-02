80ca6e072d2b246031e006f05aada3936218726a
deraklmc <gderaj98@gmail.com>
2019-09-02 19:05:30 +0530

    tuning the grass image

--- assignment-2-filtering/2/code/myMainScript.m
+++ assignment-2-filtering/2/code/myMainScript.m
@@ -4,11 +4,11 @@ clear all;
 clc;
 tic;
 %% Your code here
-img1 = load('../data/barbara.mat');
-img = double(img1.imageOrig);
-[~] = myBilateralFiltering(img, 0.9, 25, 57);
-% img = imread('../data/grass.png');
-% [~] = myBilateralFiltering(img, 10, 26, 3);
+% img1 = load('../data/barbara.mat');
+% img = double(img1.imageOrig);
+% [~] = myBilateralFiltering(img, 0.9, 25, 57);
+img = imread('../data/grass.png');
+[~] = myBilateralFiltering(img, 7, 26, 3);
 % img = imread('../data/honeycombReal.png')
 % [~] = myBilateralFiltering(img, 10, 30, 13);
 %%
