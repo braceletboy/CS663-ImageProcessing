0f256d96230f3ae98f8a168c17cd2c0b13a30020
bvsg363 <bvsg363@gmail.com>
2019-09-02 20:24:37 +0530

    3rd question tuning done

--- assignment-2-filtering/3/code/generate_patch_based_filter.m
+++ assignment-2-filtering/3/code/generate_patch_based_filter.m
@@ -24,7 +24,7 @@ for row = window_top : window_bottom
     for column = window_left : window_right
         other_patch = padded_img(row : row+patch_size-1, column : column+patch_size-1);
         filter(row-window_top+1, column-window_left+1) = ...
-            exp (-1 * (sum((center_patch - other_patch).^2, 'all')/(sigma^2)));
+            exp (-1 * (sum((center_patch - other_patch).^2, 'all')/(2 * sigma^2)));
     end
 end
 filter = filter/sum(filter, 'all');
--- assignment-2-filtering/3/code/myMainScript.m
+++ assignment-2-filtering/3/code/myMainScript.m
@@ -8,60 +8,152 @@ tic;
 img1 = "../data/barbara.mat";
 img2 = "../data/grass.png";
 img3 = "../data/honeyCombReal.png";
-input_images = [img1, img2, img3];
-
 window_size = 25;
 patch_size = 9;
-sigma_opt = [150, 150, 150];
+sigma_opt_arr = [25, 85, 90];
+%----------------------------------------------------------------------
+%----------------------------------------------------------------------
 
-%% Smoothening the input images
-for i = 1 : 1
-    if i == 1
-        img1_mat = load(input_images(i));
-        img = double(img1_mat.imageOrig);
-    else
-        img = double(imread(input_images(i)));
-    end
-    
-    [corrupted_img, smoothed_img] = myPatchBasedFiltering(img, sigma_opt(i), window_size, patch_size);
+%% Smoothening the input image1 - barbara.mat
 
-    figure;
-    sgtitle('Patch Based Filtering');
-    subplot(1,3,1), imshow(img, []);
-    title('Original');
-    colorbar;
-    subplot(1,3,2), imshow(corrupted_img, []);
-    title('Corrupted');
-    colorbar;
-    subplot(1,3,3), imshow(smoothed_img, []);
-    title('Filtered');
-    colorbar;
+% sigma_opt = sigma_opt_arr(1);
+% img_mat = load(img1);
+% img = double(img_mat.imageOrig);
+% 
+% [corrupted_img, smoothed_img] = myPatchBasedFiltering(img, sigma_opt, window_size, patch_size);
+% 
+% figure('Position', [80 80 1200 400]);
+% colormap('gray');
+% sgtitle('Patch Based Filtering');
+% subplot(1,3,1), imagesc(img);
+% title('Original');
+% colorbar;
+% subplot(1,3,2), imagesc(corrupted_img);
+% title('Corrupted');
+% colorbar;
+% subplot(1,3,3), imagesc(smoothed_img);
+% title('Filtered');
+% colorbar;
+% 
+% %% Displaying the mask used to make patches isotropic
+% gaussian_mask = fspecial('gaussian', patch_size, sigma_opt);
+% figure;
+% colormap('gray');
+% imagesc(gaussian_mask);
+% title('Mask for isotropic patches');
+% colorbar;
+% 
+% %% Optimal parameter value(sigma*) and optimal RMSD
+% rmsd_loss = sqrt(sum((smoothed_img-img).^2, 'all')/numel(img));
+% 
+% fprintf('The optimal sigma* value is %f\n', sigma_opt);
+% fprintf('The optimal RMSD value is %f\n', rmsd_loss);
+% 
+% %% RMSD values for i) 0.9*sigma and ii) 1.1*sigma
+% 
+% sigma = 0.9*sigma_opt;
+% [~, smoothed_img] = myPatchBasedFiltering(img, sigma, window_size, patch_size);
+% rmsd_loss = sqrt(sum((smoothed_img-img).^2, 'all')/numel(img));
+% fprintf('The RMSD value for 0.9*sigma is %f\n', rmsd_loss);
+% 
+% sigma = 1.1*sigma_opt;
+% [~, smoothed_img] = myPatchBasedFiltering(img, sigma, window_size, patch_size);
+% rmsd_loss = sqrt(sum((smoothed_img-img).^2, 'all')/numel(img));
+% fprintf('The RMSD value for 1.1*sigma is %f\n', rmsd_loss);
+%----------------------------------------------------------------------
+%----------------------------------------------------------------------
+
+%% Smoothening the input image2 - grass.png
+sigma_opt = sigma_opt_arr(2);
+img = double(imread(img2));
+
+[corrupted_img, smoothed_img] = myPatchBasedFiltering(img, sigma_opt, window_size, patch_size);
+
+figure('Position', [80 80 1200 400]);
+colormap('gray');
+sgtitle('Patch Based Filtering');
+subplot(1,3,1), imagesc(img);
+title('Original');
+colorbar;
+subplot(1,3,2), imagesc(corrupted_img);
+title('Corrupted');
+colorbar;
+subplot(1,3,3), imagesc(smoothed_img);
+title('Filtered');
+colorbar;
 
 %% Displaying the mask used to make patches isotropic
-    gaussian_mask = fspecial('gaussian', patch_size, sigma_opt(i));
-    figure;
-    imshow(gaussian_mask, [min(gaussian_mask,[],'all'), max(gaussian_mask,[],'all')], ...
-        'InitialMagnification', 900);
-    title('Mask for isotropic patches');
-    colorbar;
+gaussian_mask = fspecial('gaussian', patch_size, sigma_opt);
+figure;
+colormap('gray');
+imagesc(gaussian_mask);
+title('Mask for isotropic patches');
+colorbar;
 
 %% Optimal parameter value(sigma*) and optimal RMSD
-    rmsd_loss = sqrt(sum((smoothed_img-img).^2, 'all')/numel(img));
+rmsd_loss = sqrt(sum((smoothed_img-img).^2, 'all')/numel(img));
 
-    fprintf('The optimal sigma* value is %f\n', sigma_opt(i));
-    fprintf('The optimal RMSD value is %f\n', rmsd_loss);
+fprintf('The optimal sigma* value is %f\n', sigma_opt);
+fprintf('The optimal RMSD value is %f\n', rmsd_loss);
 
 %% RMSD values for i) 0.9*sigma and ii) 1.1*sigma
 
-    sigma = 0.9*sigma_opt(i);
-    [~, smoothed_img] = myPatchBasedFiltering(img, sigma, window_size, patch_size);
-    rmsd_loss = sqrt(sum((smoothed_img-img).^2, 'all')/numel(img));
-    fprintf('The RMSD value for 0.9*sigma is %f\n', rmsd_loss);
+sigma = 0.9*sigma_opt;
+[~, smoothed_img] = myPatchBasedFiltering(img, sigma, window_size, patch_size);
+rmsd_loss = sqrt(sum((smoothed_img-img).^2, 'all')/numel(img));
+fprintf('The RMSD value for 0.9*sigma is %f\n', rmsd_loss);
 
-    sigma = 1.1*sigma_opt(i);
-    [~, smoothed_img] = myPatchBasedFiltering(img, sigma, window_size, patch_size);
-    rmsd_loss = sqrt(sum((smoothed_img-img).^2, 'all')/numel(img));
-    fprintf('The RMSD value for 1.1*sigma is %f\n', rmsd_loss);
+sigma = 1.1*sigma_opt;
+[~, smoothed_img] = myPatchBasedFiltering(img, sigma, window_size, patch_size);
+rmsd_loss = sqrt(sum((smoothed_img-img).^2, 'all')/numel(img));
+fprintf('The RMSD value for 1.1*sigma is %f\n', rmsd_loss);
+%----------------------------------------------------------------------
+%----------------------------------------------------------------------
 
-end
+%% Smoothening the input image1 - honeyCombReal.png
+sigma_opt = sigma_opt_arr(3);
+img = double(imread(img3));
+
+[corrupted_img, smoothed_img] = myPatchBasedFiltering(img, sigma_opt, window_size, patch_size);
+
+figure('Position', [80 80 1200 400]);
+colormap('gray');
+sgtitle('Patch Based Filtering');
+subplot(1,3,1), imagesc(img);
+title('Original');
+colorbar;
+subplot(1,3,2), imagesc(corrupted_img);
+title('Corrupted');
+colorbar;
+subplot(1,3,3), imagesc(smoothed_img);
+title('Filtered');
+colorbar;
+
+%% Displaying the mask used to make patches isotropic
+gaussian_mask = fspecial('gaussian', patch_size, sigma_opt);
+figure;
+colormap('gray');
+imagesc(gaussian_mask);
+title('Mask for isotropic patches');
+colorbar;
+
+%% Optimal parameter value(sigma*) and optimal RMSD
+rmsd_loss = sqrt(sum((smoothed_img-img).^2, 'all')/numel(img));
+
+fprintf('The optimal sigma* value is %f\n', sigma_opt);
+fprintf('The optimal RMSD value is %f\n', rmsd_loss);
+
+%% RMSD values for i) 0.9*sigma and ii) 1.1*sigma
+
+sigma = 0.9*sigma_opt;
+[~, smoothed_img] = myPatchBasedFiltering(img, sigma, window_size, patch_size);
+rmsd_loss = sqrt(sum((smoothed_img-img).^2, 'all')/numel(img));
+fprintf('The RMSD value for 0.9*sigma is %f\n', rmsd_loss);
+
+sigma = 1.1*sigma_opt;
+[~, smoothed_img] = myPatchBasedFiltering(img, sigma, window_size, patch_size);
+rmsd_loss = sqrt(sum((smoothed_img-img).^2, 'all')/numel(img));
+fprintf('The RMSD value for 1.1*sigma is %f\n', rmsd_loss);
+%----------------------------------------------------------------------
+%----------------------------------------------------------------------
 toc
