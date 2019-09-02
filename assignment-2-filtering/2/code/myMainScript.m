bc1247672ba28a210d0e1b17c7aa09d59f861107
deraklmc <gderaj98@gmail.com>
2019-09-02 17:13:43 +0530

    tuning the honeycomb image

--- assignment-2-filtering/2/code/myMainScript.m
+++ assignment-2-filtering/2/code/myMainScript.m
@@ -6,5 +6,7 @@ tic;
 %% Your code here
 img = imread('../data/grass.png');
 [~] = myBilateralFiltering(img, 10, 26, 3);
+img = imread('../data/honeycombReal.png')
+[~] = myBilateralFiltering(img, 10, 30, 13);
 %%
 toc;
