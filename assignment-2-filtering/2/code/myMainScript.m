0042d2d0bf32046b60f5364e6d2292d219914add
deraklmc <gderaj98@gmail.com>
2019-09-02 19:01:56 +0530

    tuning the barbara image

--- assignment-2-filtering/2/code/myMainScript.m
+++ assignment-2-filtering/2/code/myMainScript.m
@@ -4,9 +4,12 @@ clear all;
 clc;
 tic;
 %% Your code here
-img = imread('../data/grass.png');
-[~] = myBilateralFiltering(img, 10, 26, 3);
-img = imread('../data/honeycombReal.png')
-[~] = myBilateralFiltering(img, 10, 30, 13);
+img1 = load('../data/barbara.mat');
+img = double(img1.imageOrig);
+[~] = myBilateralFiltering(img, 0.9, 25, 57);
+% img = imread('../data/grass.png');
+% [~] = myBilateralFiltering(img, 10, 26, 3);
+% img = imread('../data/honeycombReal.png')
+% [~] = myBilateralFiltering(img, 10, 30, 13);
 %%
 toc;
