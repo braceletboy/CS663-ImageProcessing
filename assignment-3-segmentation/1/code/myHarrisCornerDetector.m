7b7f79c4de9f96acb6d253475e1b5dabc93a46df
bvsg363 <bvsg363@gmail.com>
2019-09-08 22:51:57 +0530

    tuning ques1

--- assignment-3-segmentation/1/code/myHarrisCornerDetector.m
+++ assignment-3-segmentation/1/code/myHarrisCornerDetector.m
@@ -1,4 +1,19 @@
-function corners = myHarrisCornerDetector(eig_min, eig_max, corn_k, threshlod)
+function corners = myHarrisCornerDetector(eig_min, eig_max, corn_k, threshold)
+
+    [rows, columns] = size(eig_min);
     %% cornerness
-    corn_prop = eig_min.*eig_max - k.*((eig_min + eig_max).^2);
+    corners = eig_min.*eig_max - corn_k*((eig_min + eig_max).^2);
+    corners(corners < threshold) = 0;
+    
+    %% Non-Maximal supression
+    for i=2:2:rows-2
+        for j=2:2:columns-2
+            corners(i-1:i+1,j-1:j+1)=(corners(i-1:i+1,j-1:j+1) == ...
+                            max(max(corners(i-1:i+1,j-1:j+1))))*max(max(corners(i-1:i+1,j-1:j+1)));
+        end
+    end
+    
+    %% Thresholding
+%     corners(corners >= threshold) = 1;
+%     corners(corners < threshold) = 0;
 end
--- assignment-3-segmentation/1/code/myMainScript.m
+++ assignment-3-segmentation/1/code/myMainScript.m
@@ -1,24 +1,33 @@
 %% MyMainScript
 
 tic;
-%% Your code here
+%% Variable initializations
+
+% Loaidng the input image
 img_mat = load('../data/boat.mat');
 img = double(img_mat.imageOrig);
-img = img/max(max(img));
-sigma1 = 0.25;
-sigma2 = 10;
-corn_k = 0.15;
-threshlod = 0.05;
+img = img / max(max(img));
+
+% Shifting and rescaling
+img = img - min(min(img));
+img = img / max(max(img));
+
+% parameters to be tuned
+sigma1 = 0.05;
+sigma2 = 0.5;
+corn_k = 0.182;
+threshlod = 0.006;
+window_size = 5;
 
 %% 1st Gaussian Smoothing -- noise removal
-img=imgaussfilt(img,sigma1);
+img = imgaussfilt(img,sigma1);
 
-%% Imgae derivatives along X and Y using Sobel operator
+%% Image derivatives along X and Y using Sobel operator
 [Ix, Iy] = imgradientxy(img, 'sobel');
 figure('name','ImageDerivatives','Position', [100 100 1200 400]);
 subplot(1,2,1),imshow(Ix);
 colormap('gray');
-title('Y - Derivative of img');
+title('X - Derivative of img');
 subplot(1,2,2),imshow(Iy);
 colormap('gray');
 title('Y - Derivative of img');
@@ -26,24 +35,52 @@ title('Y - Derivative of img');
 Ix2 = Ix.^2;
 Iy2 = Iy.^2;
 Ixy = Ix.*Iy;
-trace = Ix2 + Iy2;
-det = Ix2.*Iy2 - Ixy.^2;
+
+output_Ix2=imgaussfilt(Ix2, sigma2, 'Filtersize', window_size);
+output_Iy2=imgaussfilt(Iy2, sigma2, 'Filtersize', window_size);
+output_Ixy=imgaussfilt(Ixy, sigma2, 'Filtersize', window_size);
+
+trace = output_Ix2 + output_Iy2;
+det = output_Ix2.*output_Iy2 - output_Ixy.^2;
 
 eig_min=(trace/2)-((trace/2).^2 - det).^(1/2);
 eig_max=(trace/2) +((trace/2).^2 - det).^(1/2);
 
 figure('name','Eigen Value Images','Position', [100 100 1200 400]);
+
 subplot(1,2,1),imshow(eig_min/max(max(eig_min)));
-colormap('gray');
+colorbar;
+colormap('jet');
 title('Eigen minimum');
+
 subplot(1,2,2),imshow(eig_max/max(max(eig_max)));
-colormap('gray');
+colorbar;
+colormap('jet');
 title('Eigen maximum');
 
-%%  Harris corner-ness measure
+%%  Harris corner-ness measure and displaying the corners
 corners = myHarrisCornerDetector(eig_min, eig_max, corn_k, threshlod);
+
+% Inserting markers
+[rows, columns] = find(corners > 0);
+marker_positions = cat(2, rows, columns);
+corners_marked = insertMarker(img, marker_positions, 'color', 'green', 'Size', 3);
+
 figure('name','Corner-ness','Position', [100 100 1200 400]);
-imshow(corners);
+
+subplot(1,2,1),imshow(corners);
+colorbar;
 colormap('gray');
-title('Eigen minimum');
+title('corners');
+
+subplot(1,2,2),imshow(corners_marked);
+colorbar;
+colormap('gray');
+title('corners-marked');
+
+%% Reporting the parameters
+fprintf('std for smoothing(sigma1) is %f\n', sigma1);
+fprintf('std for Ix, Iy(sigma2) is %f\n', sigma2);
+fprintf('k value in corner-ness measure(k) is %f\n', corn_k);
+
 toc;
