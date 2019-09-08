cbefb56fbf9be88b9460f719e58f6822c65e1dde
bvsg363 <bvsg363@gmail.com>
2019-09-09 01:33:24 +0530

    working on ques2

--- assignment-3-segmentation/2/code/myMainScript.m
+++ assignment-3-segmentation/2/code/myMainScript.m
@@ -4,16 +4,19 @@ close all;
 clc;
 tic;
 %% Parameters initialization
+img = imread('../data/flower.png');
+
 h_c = 0.05;
 h_s = 10;
-iterations = 20;
+iterations = 5;
+learning_rate = 0.9;
 %% Mean shift segmentation
-segmented_img = myMeanShiftSegmentation(0.1, 11, 25, 4);
+segmented_img = myMeanShiftSegmentation(learning_rate, iterations, h_s, h_c);
 figure;
 subplot(1,2,1), imagesc(img);
 title('Original');
 colorbar;
-subplot(1,2,2), imagesc(segmented_img);
+subplot(1,2,2), imagesc(uint8(segmented_img));
 title('Segmented');
 colorbar;
 
