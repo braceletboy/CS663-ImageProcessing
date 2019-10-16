aab86271eae4ae79c2949af58ca651981117672b
bvsg363 <bvsg363@gmail.com>
2019-10-16 18:01:29 +0530

    working on Ques 2

--- assignment-4_SVD_FaceRecognition/2/code/myMainScript.m
+++ assignment-4_SVD_FaceRecognition/2/code/myMainScript.m
@@ -1,6 +1,30 @@
-%% MyMainScript
+%% MyMainScript -- Reconstruction of a face image from the ORL database using svd function
 
 tic;
-%% Your code here
+%% Input data
+orl_dir = '../../../orl/'; % make sure that the path ends with '/'
+image_path = ' ';
+img = imread(image_path);
+img = double(img)/255;
+img = img(:);
 
+k_vals = [2, 10, 20, 50, 75, 100, 125, 150, 175];
+
+%% Extracting K eigen values
+% Step1: Generate datasets
+[orl_train_dataset, orl_test_dataset] = get_dataset(orl_dir, "ORL");
+% Step2: Get Eigen matrix
+orl_eigen_matrix = get_eigen_matrix(orl_train_dataset, true);
+
+
+
+%% Displaying the eigen faces
+figure;
+for i = 1:25
+    eig_face = reshape(V(:, i), 192, 168);
+    subplot(5,5,i);
+    imagesc(eig_face);
+    colormap('gray');
+    title(['Eigenface #' num2str(i)]);
+end
 toc;
