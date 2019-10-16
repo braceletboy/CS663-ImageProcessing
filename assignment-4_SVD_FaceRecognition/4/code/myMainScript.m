0702e81081a05fa728e6ffa59aa6145198f35a94
bvsg363 <bvsg363@gmail.com>
2019-10-16 16:01:47 +0530

    Ques 4 completed

--- assignment-4_SVD_FaceRecognition/4/code/myMainScript.m
+++ assignment-4_SVD_FaceRecognition/4/code/myMainScript.m
@@ -1,10 +1,21 @@
 %% MyMainScript
-
 tic;
-%% Your code here
-A = reshape((1:12), 4, 3);
-[U, S, V] = mySVD(A);
+%% Input matrix A
+A = reshape((1:12), 3, 4);
+[U, S, V] = MySVD(A);
+
+%% Output
+fprintf("Left singular vector U\n");
+display(U);
+fprintf("Right singular vector V\n");
+display(V);
+fprintf("Singular values matrix S\n");
+display(S);
+
+%% Crosscheck with inbuilt svd function
+fprintf("MySVD(A) = \n");
 disp(U*S*V');
 [U1, S1, V1] = svd(A, 'econ');
+fprintf("svd(A) = \n");
 disp(U1*S1*V1');
 toc;
--- assignment-4_SVD_FaceRecognition/4/code/mySVD.m
+++ assignment-4_SVD_FaceRecognition/4/code/mySVD.m
@@ -1,4 +1,5 @@
-function [U, S, V] = mySVD(A)
+function [U, S, V] = MySVD(A)
+% Here dimensions of U -> m*m, S -> m*n, V -> n*n
 [U, D] = eig(A*A');
 [~, Indices] = sort(diag(D), 'descend');
 U = U(:, Indices);
@@ -7,16 +8,23 @@ U = U(:, Indices);
 [~, Indices] = sort(diag(D), 'descend');
 V = V(:, Indices);
 
-% A=U*S*V' ==> U'*A*V = U'*U*S*V'*V = S. 
+% Observation
+% A = U*S*V' ==> U'*A*V = U'*U*S*V'*V = S.
+% Here S may contain negative values, hence for obtaining the standard
+% results(i.e, S contains positive eigen values) we take the negative
+% vectors of U corresponding to negative values in S.
 S = U'*A*V;
 [m,n] = size(S);
+
 for i=1:1:min(m,n)
     if(S(i,i)<0)
-        U(:,i)=-U(:,i);
+        U(:,i) = -U(:,i);
     end 
 end
 S = abs(S);
 
+% Other approach --> taking only the non-zero eigen values('r' no of values) and
+% corresponding vectors, dimensions of U -> m*r, V -> n*r, S -> r*r
 % [U, D] = eig(A*A');
 % Indices = find(fix(diag(D)));
 % D = diag(D);
